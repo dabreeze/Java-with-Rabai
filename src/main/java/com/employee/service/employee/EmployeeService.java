f8e60bfcd038768672cf37b7634f32c65f58b1f1
foreverchild <harrisonogbonnaya@gmail.com>
2022-02-13 21:06:51 +0100

    employee service

---
+++ src/main/java/com/employee/service/employee/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.employee.service.employee;
+
+import com.employee.data.dto.EmployeeDto;
+import com.employee.data.models.Employee;
+import com.employee.web.exceptions.BusinessLogicException;
+import com.employee.web.exceptions.EmployeeDoesNotExistException;
+import com.github.fge.jsonpatch.JsonPatch;
+
+
+
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployee();
+    Employee findEmployeeById (Long employeeId) throws EmployeeDoesNotExistException;
+    Employee createEmployee(EmployeeDto employeeDto) throws BusinessLogicException;
+    Employee updateEmployeeDetails(Long employeeId, JsonPatch employeePatch) throws BusinessLogicException;
+}

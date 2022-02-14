069a25d17a52157004ecb4357cfac30f381ea4e7
foreverchild <harrisonogbonnaya@gmail.com>
2022-02-14 13:57:04 +0100

    commit to models

---
+++ src/main/java/com/employee/data/dto/EmployeeDto.java
@@ -0,0 +1,13 @@
+package com.employee.data.dto;
+
+import lombok.Data;
+
+@Data
+public class EmployeeDto {
+    private String role;
+    private  String firstName;
+    private String email;
+    private String lastName;
+
+
+}
---
+++ src/main/java/com/employee/data/models/Employee.java
@@ -0,0 +1,20 @@
+package com.employee.data.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String role;
+    @Column(nullable = false, unique = false)
+    private String email;
+    private LocalDateTime dateCreated;
+}

1a5a2eecc7648cbc3c64892c55bac8f695afff4d
foreverchild <harrisonogbonnaya@gmail.com>
2022-02-21 14:32:07 +0100

    employee

--- src/main/java/com/employee/data/models/Employee.java
+++ src/main/java/com/employee/data/models/Employee.java
@@ -1,20 +1,27 @@
 package com.employee.data.models;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@RequiredArgsConstructor
 @Data
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private String id;
     private String firstName;
     private String lastName;
     private String role;
     @Column(nullable = false, unique = false)
     private String email;
     private LocalDateTime dateCreated;
+
+    public Employee(String id ){
+        this.id = id;
+    }
+
 }
